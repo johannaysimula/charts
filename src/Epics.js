78c8f2a90a9debb4a37477a665d96767566b08e6
Jo Hannay <johannay@simula.no>
2022-09-16 15:53:34 +0200

    Jo

--- src/Epics.js
+++ src/Epics.js
@@ -12,7 +12,7 @@ const API_HOST = "https://bcdam-json-server.herokuapp.com";
 //const CALC_HOST = "http://bcdam.ddns.net:5000";
 const BACKLOG_API_URL = `${API_HOST}/backlog`;
 const PARAMETERS_API_URL = `${API_HOST}/parameters`;
-const CALCULATION_API_URL = `${CALC_HOST}/inventory`;
+const CALCULATION_API_URL = `${CALC_HOST}`;
 
 function Epics() {
 
