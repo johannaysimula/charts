02a9f105108226fec33f5864c6eeb0ba7065958d
Jo Hannay <johannay@simula.no>
2022-11-08 11:09:52 +0100

    Jo

--- src/ScheduleChart.jsx
+++ src/ScheduleChart.jsx
@@ -141,7 +141,7 @@ export default function ScheduleChart({ optimization_criterion }) {
         const socket = io(`${ASP_HOST}`, {
             transports: ["websocket"],
             cors: {
-              origin: "wss://bcdam.herokuapp.com:35651/ws",
+              origin: "http://localhost:3000",
             },
 
         });
