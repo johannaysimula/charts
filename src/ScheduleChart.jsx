bc1b86d23c547f2eaf00689552c802e696294762
Jo Hannay <johannay@simula.no>
2022-11-10 23:13:33 +0100

    gfd

--- src/ScheduleChart.jsx
+++ src/ScheduleChart.jsx
@@ -170,7 +170,7 @@ export default function ScheduleChart({ optimization_criterion }) {
 
         //console.log("origin: ", "wss://bcdam.herokuapp.com:" + process.env.REACT_APP_PORT + "/ws");
 
-        socket.emit('preparedata', optimization_criterion);
+        socket.emit('preparedata');
 
         //setLoading(false)
 
