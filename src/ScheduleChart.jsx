08f825b2a5103c6122ef2b3101503745cbbb20f6
Jo Hannay <johannay@simula.no>
2022-11-11 19:34:56 +0100

    sfdfa

--- src/ScheduleChart.jsx
+++ src/ScheduleChart.jsx
@@ -142,8 +142,8 @@ export default function ScheduleChart({ optimization_criterion }) {
 
 
         const manager = new Manager(`${ASP_HOST}`);
-        manager.engine.port=44505;
-        manager.opts.port=44505;
+        manager.engine.port=16856;
+        manager.opts.port=16856;
 
         console.log("manager: ", manager);
 
