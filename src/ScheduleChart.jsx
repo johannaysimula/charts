6e2c5f8f8c77fb67a0b46210627ca28947c936d3
Jo Hannay <johannay@simula.no>
2022-11-12 23:39:23 +0100

    dfsgs

--- src/ScheduleChart.jsx
+++ src/ScheduleChart.jsx
@@ -189,7 +189,7 @@ console.log(`${ASP_HOST}:` + ASPHostPort)
         const socket = manager.socket('/0.0.0.0', {
             transports: ["websocket"],
             cors: {
-                origin: ["https://bcdam.herokuapp.com:3001/"],
+                origin: ["wss://bcdam.herokuapp.com:3001/ws"],
             },
 
         });
