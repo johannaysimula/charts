8e836c52e77ebb9bf286094d6e55f22a826454d1
Jo Hannay <johannay@simula.no>
2022-11-14 22:09:19 +0100

    sf

--- src/ScheduleChart.jsx
+++ src/ScheduleChart.jsx
@@ -8,7 +8,7 @@ const API_HOST = "https://bcdam-json-server.herokuapp.com";
 //const API_HOST = "http://bcdam.ddns.net:3000";
 const BACKLOG_API_URL = `${API_HOST}/backlog`;
 //const ASP_HOST = "http://localhost:5000/receiver";
-const ASP_HOST = "https://bcdam-python-asp-service-extra.herokuapp.com/0.0.0.0";
+const ASP_HOST = "https://bcdam-python-asp-service-extra.herokuapp.com/socket.io";
 const ASP_HOST_GETPORT = "https://bcdam-python-asp-service-extra.herokuapp.com/getport"; //localhost: no 0.0.0.0., then initial connect and discob\nnecy with emptying quueue s not there.
 //const ASP_HOST = "http://localhost:5001/"
 const POSE_PROBLEM_URL = `${ASP_HOST}/problem`;
@@ -190,7 +190,7 @@ export default function ScheduleChart({ optimization_criterion }) {
         const socket = manager.socket('/', {
             transports: ["websocket"],
             cors: {
-                origin: ["wss://bcdam.herokuapp.com/0.0.0.0:3000/ws"],
+                origin: ["wss://bcdam.herokuapp.com/socket.io/3000/ws"],
             },
 
         });
